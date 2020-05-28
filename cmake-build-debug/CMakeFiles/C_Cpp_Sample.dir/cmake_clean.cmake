858aa5a4e4089a1e579b9297bf3156984169f77d
mayisheng <13104862372@163.com>
2020-05-28 20:17:22 +0800

    C and Cpp Sample

---
+++ cmake-build-debug/CMakeFiles/C_Cpp_Sample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Cpp_Sample.dir/main.cpp.o"
+  "C_Cpp_Sample"
+  "C_Cpp_Sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_Cpp_Sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
