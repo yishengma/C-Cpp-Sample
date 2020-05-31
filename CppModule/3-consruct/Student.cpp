61c0e91c722c8ed48448199173f23c099d1fb373
mayisheng <13104862372@163.com>
2020-05-31 16:01:50 +0800

    C++ new/delete和malloc/free

--- CppModule/3-consruct/Student.cpp
+++ CppModule/3-consruct/Student.cpp
@@ -3,6 +3,7 @@
 //
 
 
+#include <cstdlib>
 #include "Student.h"
 
 void Student::setAge(int age) {
@@ -19,14 +20,16 @@ char* Student::getName() {
 }
 
 
-void main() {
-    Student stu1 ;//1.默认调用空参数的构造函数
+int main() {
+    Student stu1 ;//1.默认调用空参数的构造函数，在栈内开辟的内存
 
-    Student stu2("ABC",10);//2.调用两个参数的构造函数
+    Student stu2("ABC",10);//2.调用两个参数的构造函数，，在栈内开辟的内存
 
-    Student* stu3 = new Student("ABC",20);//3.使用 new 关键字
+    Student* stu3 = new Student("ABC",20);//3.使用 new 关键字，会调用构造函数和析构函数
+    delete stu3;//new 和 delete 配套
 
-    Student* stu4 = (Studen*)malloc(sizeof(Student));//4.使用malloc的方式，并没有调用空参数的构造函数
+    Student* stu4 = (Student*)malloc(sizeof(Student));//4.使用malloc的方式，并没有调用空参数的构造函数和析构函数
+    free(stu4);//malloc 和 free 配套
 
 
 
