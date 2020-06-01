bd8f59e088f26b0e9e05dfbe01345d4549416daa
mayisheng <13104862372@163.com>
2020-06-01 16:32:04 +0800

    C++ 模板类

--- CppModule/14-template/Sample.cpp
+++ CppModule/14-template/Sample.cpp
@@ -10,6 +10,7 @@ double add(double num1, double num2) {
     return num1 + num2;
 }
 
+//当普通函数和模板函数同时存在的时候，优先会调用普通函数
 float add(float num1, float num2) {
     return num1 + num2;
 }
@@ -20,4 +21,36 @@ T add(T num1, T num2) {
     return num1 + num2;
 }
 
+//so是很难被反编译，比java安全
+//1.编译的时候，c和c++编译器会对 cpp ,c 文件做一些优化（模板函数）
+//2.java 有small -> class ，也会生成c/c++ 能够认识的一套代码
+//3.链接 obj , .等文件，链接其他模板 cmake 中的链接
+//4.最会就会生成 .dll , .so .exe 文件
 
+
+
+template<class T>
+class Callback{
+public:
+    void onError(){
+
+    }
+
+    void onSucceed(T result){
+
+    }
+};
+
+class HttpCallback : public Callback<int> {
+
+};
+
+template<class T>
+class HttpsCallback : public Callback<T> {
+
+};
+
+int main() {
+    HttpsCallback<int>* callback = new HttpsCallback<int>();
+    callback->onError();
+}
