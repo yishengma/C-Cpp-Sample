30feb90b51b179d9bb4603afcd6bb92e007a82e2
mayisheng <13104862372@163.com>
2020-06-17 16:46:19 +0800

    c++ 强智能指针的交叉引用问题

--- CppModule/25-intelligent-pointer/Sample2.cpp
+++ CppModule/25-intelligent-pointer/Sample2.cpp
@@ -8,6 +8,8 @@
 //智能指针-》 资源的时候 -》引用计数+1
 //智能指针-》不使用资源的时候-》引用计数-1， 计数为0 ，资源释放
 
+#include <memory>
+#include <iostream>
 
 //对智能指针引用计数的类
 template<typename T>
@@ -82,3 +84,60 @@ private:
     //指向资源引用计数对象的指针
     RefCnt<T>* pRefCnt;
 };
+
+//share_ptr:强智能指针，可以资源的引用计数
+//weak_ptr:弱智能指针，不会改变资源的引用计数
+
+//强智能指针循环引用（交叉引用）是什么问题？什么结果
+//交叉引用，造成资源泄露
+class A;
+class B {
+public:
+    B() {
+
+    }
+
+    virtual ~B() {
+
+    }
+    std::shared_ptr<A> ptr;
+    std::weak_ptr<A> ptr1;
+
+};
+class A {
+public:
+    A() {
+
+    }
+
+    virtual ~A() {
+
+    }
+    std::shared_ptr<B> ptr;
+    std::weak_ptr<B> ptr1;
+
+
+};
+
+int main() {
+    std::shared_ptr<A> pa(new A);
+    std::shared_ptr<B> pb(new B);
+
+    //交叉引用，造成资源泄露
+    pa->ptr = pb;
+    pb->ptr = pa;
+
+    //若智能指针，不会改变引用计数，所以可以释放
+    //弱只能指针只是观察资源，不能使用资源
+    //即不可以通过*p 访问
+    pa->ptr1 = pb;
+    pb->ptr1 = pa;
+
+    //因此两种指针的方式
+    //引用对象的地方使用弱智能指针
+    //定义对象的地方使用强智能指针
+
+    std::cout << pa.use_count() << std::endl;
+    std::cout << pb.use_count() << std::endl;
+
+}
