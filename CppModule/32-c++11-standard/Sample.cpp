f6da34b665fcf9161fd1b06ce29c5d0ca80d0752
mayisheng <13104862372@163.com>
2020-06-23 19:49:05 +0800

    c++ 11 新特性

---
+++ CppModule/32-c++11-standard/Sample.cpp
@@ -0,0 +1,41 @@
+//
+// Created by 海盗的帽子 on 2020/6/23.
+//
+
+/**
+ C++ 标准相关
+ 一.关键字和语法
+ auto:可以根据右值，推导出右值的类型
+ nullptr : 给指针专用（能够和整数区分） #define NULL 0
+ foreach:可以遍历数组，容器等，底层通过指针或者迭代器来实现的
+
+ 右值引用：move 移动语义和 forward 类型完美转发函数
+
+ 模版的一个新特性 typename... A  表示类型的可变参数
+
+ 二.绑定器和函数对象
+ function:函数对象
+ bind:绑定器 bind1st 和 bind2nd + 二元函数对象-》 一元函数对象
+ lambda 表达式
+
+ 三.智能指针
+ share_ptr 和 weak_ptr
+
+ 四.容器
+ set 和 map 红黑树
+ unordered_set
+ unordered_map 哈希表
+
+ array 数组 ， vector
+ forward_list , list
+
+
+  五.c++ 支持语言级别的多线程编程
+    之前用的是系统级别的
+  windows:CreateThread
+  Linux:pthread_create
+
+
+
+
+ */
